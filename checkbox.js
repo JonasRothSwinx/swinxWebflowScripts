62d0878e9b70ac6da0831bfdb306c92980af690e
Jonas <jonasroth1@gmail.com>
2025-01-21 16:32:49 +0100

    fix: add package selection handling to checkbox behavior

--- checkbox.js
+++ checkbox.js
@@ -1,8 +1,20 @@
 // alert("AAAAAAAH");
 console.log("I'm here!");
 $(document).ready(function () {
+    const packageSelect = $(".packageSelect");
     const $checkboxWrapper = $("div.stylecheckboxes");
     console.log($checkboxWrapper);
+    console.log(packageSelect);
+    let maxOptions = 1;
+    packageSelect.on("change", function () {
+        console.log("I'm here!");
+        const selectedOption = $(this).find("option:selected");
+        console.log(selectedOption);
+        maxOptions = parseInt(selectedOption.data("max-options"));
+        console.log(maxOptions);
+        $checkboxWrapper.find("input[type=checkbox]").prop("checked", false);
+        $checkboxWrapper.find("label").css({ opacity: "", "pointer-events": "" });
+    });
 
     $checkboxWrapper.find("input[type=checkbox]").on("change", function () {
         if ($checkboxWrapper.find("input[type=checkbox]:checked").length > 3) {
