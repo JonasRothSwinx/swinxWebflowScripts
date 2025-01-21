79484c1eb49785f3d55cd9231de4ae1936efa51e
Jonas <jonasroth1@gmail.com>
2025-01-21 16:13:19 +0100

    add checkbox component

---
+++ checkbox.js
@@ -0,0 +1,20 @@
+alert("AAAAAAAH");
+console.log("I'm here!");
+$(document).ready(function () {
+    var $checkboxWrapper = $("div.styleCheckBoxWrapper1");
+
+    $checkboxWrapper.find("input[type=checkbox]").on("change", function () {
+        if ($checkboxWrapper.find("input[type=checkbox]:checked").length > 3) {
+            $(this).prop("checked", false).change();
+        }
+
+        if ($checkboxWrapper.find("input[type=checkbox]:checked").length === 3) {
+            $checkboxWrapper
+                .find("input[type=checkbox]:not(:checked)")
+                .closest("label")
+                .css({ opacity: "0.5", "pointer-events": "none" });
+        } else {
+            $checkboxWrapper.find("label").css({ opacity: "", "pointer-events": "" });
+        }
+    });
+});
